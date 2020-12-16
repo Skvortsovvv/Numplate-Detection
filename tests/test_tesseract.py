2421d452d89c09b7b21456b46fff3584f70d25bc
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-12-16 23:04:40 +0300

    Update test_tesseract.py

--- tests/test_tesseract.py
+++ tests/test_tesseract.py
@@ -8,9 +8,9 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\projects\Numplate-Detection\Tessera
 class TestTesseract(unittest.TestCase):
 
     def test_reading(self):
-        self.assertEqual(Tess.Reading(1), "B23KA50")
-        self.assertEqual(Tess.Reading(2), "A424A0178")
-        self.assertEqual(Tess.Reading(3), "B410B0722")
-        self.assertEqual(Tess.Reading(4), "C308AY799")
-        self.assertEqual(Tess.Reading(5), "0474YR152")
-        self.assertEqual(Tess.Reading(6), "P128YY172")
+        self.assertEqual(Tess.Reading("tests/test_images/1"), "B23KA50")
+        self.assertEqual(Tess.Reading("tests/test_images/2"), "A424A0178")
+        self.assertEqual(Tess.Reading("tests/test_images/3"), "B410B0722")
+        self.assertEqual(Tess.Reading("tests/test_images/4"), "C308AY799")
+        self.assertEqual(Tess.Reading("tests/test_images/5"), "0474YR152")
+        self.assertEqual(Tess.Reading("tests/test_images/6"), "P128YY172")
