3d81a21ab0a9daef06e20e3ae7396d1558cb7efe
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-12-16 22:36:53 +0300

    Create test_tes.py

---
+++ Source/test/test_tes.py
@@ -0,0 +1,15 @@
+import unittest
+from Source import Tess
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\project\Tesseract\tesseract.exe'
+
+class TestTesseract(unittest.TestCase):
+
+    def test_reading(self):
+        self.assertEqual(Tess.Reading(1), "B23KA50")
+        self.assertEqual(Tess.Reading(2), "A424A0178")
+        self.assertEqual(Tess.Reading(3), "B410B0722")
+        self.assertEqual(Tess.Reading(4), "C308AY799")
+        self.assertEqual(Tess.Reading(5), "0474YR152")
+        self.assertEqual(Tess.Reading(6), "P128YY172")
