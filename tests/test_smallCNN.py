2ba01d4813cd37820bf27e75b3cda963b630b277
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-12-17 14:25:49 +0300

    Create test_smallCNN.py

---
+++ tests/test_smallCNN.py
@@ -0,0 +1,20 @@
+import unittest
+from Source.Prediction import predicting
+
+class Test_SmallCNN(unittest.TestCase):
+
+    def test_true(self):
+        self.assertEqual(predicting("test_images/1.jpg"), 1)
+        self.assertEqual(predicting("test_images/2.jpg"), 1)
+        self.assertEqual(predicting("test_images/3.jpg"), 1)
+        self.assertEqual(predicting("test_images/4.jpg"), 1)
+        self.assertEqual(predicting("test_images/5.jpg"), 1)
+        self.assertEqual(predicting("test_images/6.jpg"), 1)
+
+    def test_false(self):
+        self.assertEqual(predicting("test_images/1n.jpg"), 0)
+        self.assertEqual(predicting("test_images/2n.jpg"), 0)
+        self.assertEqual(predicting("test_images/3n.jpg"), 0)
+        self.assertEqual(predicting("test_images/4n.jpg"), 0)
+        self.assertEqual(predicting("test_images/5n.jpg"), 0)
+        self.assertEqual(predicting("test_images/6n.jpg"), 0)
